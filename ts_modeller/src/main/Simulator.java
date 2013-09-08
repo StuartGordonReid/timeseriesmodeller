5e37623f978dd92642e4d76a95f6f2af4fbc8d3e
Stuart Reid <stuart@sinkwa.com>
2013-09-08 21:12:23 +0200

    first successfullish run

--- ts_modeller/src/main/Simulator.java
+++ ts_modeller/src/main/Simulator.java
@@ -7,6 +7,7 @@
 package main;
 
 import java.io.File;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import org.encog.ml.CalculateScore;
 import org.encog.ml.data.MLDataSet;
@@ -27,20 +28,24 @@ import pso.FFNNParticle;
 public class Simulator {
 
     String[] countries;
+    DecimalFormat df = new DecimalFormat("#0.0000#");
 
     Simulator() {
-        countries = new String [] {
-        "/home/stuart/stuartgordonreid@gmail.com/Time series/timeseriesmodeller/ts_modeller/src/data/BRAZIL_train.egb",
-        "/home/stuart/stuartgordonreid@gmail.com/Time series/timeseriesmodeller/ts_modeller/src/data/RUSSIA_train.egb",
-        "/home/stuart/stuartgordonreid@gmail.com/Time series/timeseriesmodeller/ts_modeller/src/data/INDIA_train.egb",
-        "/home/stuart/stuartgordonreid@gmail.com/Time series/timeseriesmodeller/ts_modeller/src/data/CHINA_train.egb",
-        "/home/stuart/stuartgordonreid@gmail.com/Time series/timeseriesmodeller/ts_modeller/src/data/SOUTHAFRICA_train.egb"};
+        countries = new String[]{
+            "/home/stuart/stuartgordonreid@gmail.com/Time series/timeseriesmodeller/ts_modeller/src/data/BRAZIL_train.egb",
+            "/home/stuart/stuartgordonreid@gmail.com/Time series/timeseriesmodeller/ts_modeller/src/data/RUSSIA_train.egb",
+            "/home/stuart/stuartgordonreid@gmail.com/Time series/timeseriesmodeller/ts_modeller/src/data/INDIA_train.egb",
+            "/home/stuart/stuartgordonreid@gmail.com/Time series/timeseriesmodeller/ts_modeller/src/data/CHINA_train.egb",
+            "/home/stuart/stuartgordonreid@gmail.com/Time series/timeseriesmodeller/ts_modeller/src/data/SOUTHAFRICA_train.egb"};
     }
 
     public void start() {
-        for (int i = 0; i < 1; i++) {
-            
+        for (int i = 0; i < 5; i++) {
+            System.out.println("==============================================================");
             MLDataSet country = EncogUtility.loadEGB2Memory(new File(countries[i]));
+
+            double[] mean = {2.82889, 0.66099, 6.53745, 10.19651, 2.06494};
+            double[] stdev = {2.26924, 6.90071, 2.11619, 2.22157, 2.61786};
 
             FFNNParticle nn = new FFNNParticle(22, 30, 1);
             CalculateScore score = new TrainingSetScore(country);
@@ -54,9 +59,29 @@ public class Simulator {
             trainMain.addStrategy(stop);
 
             int epoch = 0;
+            int repeated = 0;
+            double prev_sse = Double.MAX_VALUE;
+
             while (!stop.shouldStop()) {
+                if (epoch % 100 == 0) {
+                    //nn.getParticle().
+
+                    double sse = trainMain.getError();
+                    //double zed_score = (sse - mean[i]) / stdev[i];
+                    //double prediction = mean[i] + (zed_score * stdev[i]);
+                    
+                    System.out.println(df.format(sse));
+
+                    if (sse == prev_sse) {
+                        repeated++;
+                        if (repeated >= 10) {
+                            break;
+                        }
+                    }
+                    
+                    prev_sse = sse;
+                }
                 trainMain.iteration();
-                System.out.println(trainMain.getError());
                 epoch++;
             }
         }
